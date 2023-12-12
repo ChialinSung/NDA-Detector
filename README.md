8d149c63737ed58f1f1f0d06b4a86408387e8fbd
ChialinSung <songjialin35@163.com>
2023-12-12 21:01:29 +0800

    add test_wncd, modify readme

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
 # NDA-Detector
 
 ## Introduction
-This repository is a pytorch implementation of the paper "xxx", the code is a copy from the private repository of our engineering project, the code will be made public one by one after removing sensitive information.
+This repository is a pytorch implementation of the paper "NDA-Detector: CNN-based Multi-Scale Detector for Concealed Defect Detection in Insulation Products", the code is a copy from the private repository of our engineering project, the code will be made public after removing sensitive information.
+
 ## Timeline:
-Project creation date: May 8, 2023
+Project creation date: 5/8/2023
+
 Creation of this repository: 11/14/2023
+
+Article Submitted: 12/12/2023
+
 
---
+++ test_wncd.py
@@ -0,0 +1,41 @@
+'''
+weighted Neighbor Connection Decoder
+'''
+from test_network import BasicConv2d
+import torch
+import torch.nn as nn
+class NeighborConnectionDecoder(nn.Module):
+    def __init__(self, channel):
+        super(NeighborConnectionDecoder, self).__init__()
+        self.upsample = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+        self.conv_upsample1 = BasicConv2d(channel, channel, 3, padding=1)
+        self.conv_upsample2 = BasicConv2d(channel, channel, 3, padding=1)
+        self.conv_upsample3 = BasicConv2d(channel, channel, 3, padding=1)
+        self.conv_upsample4 = BasicConv2d(channel, channel, 3, padding=1)
+        self.conv_upsample5 = BasicConv2d(2*channel, 2*channel, 3, padding=1)
+
+        self.conv_concat2 = BasicConv2d(2*channel, 2*channel, 3, padding=1)
+        self.conv_concat3 = BasicConv2d(3*channel, 3*channel, 3, padding=1)
+        self.conv4 = BasicConv2d(3*channel, 3*channel, 3, padding=1)
+        self.conv5 = nn.Conv2d(3*channel, 1, 1)
+
+    def forward(self, x1, x2, x3, m1, m2, m3):
+        '''
+            x1 -> f'5   m1 -> gama
+            x2 -> f'4   m2 -> beta
+            x3 -> f'3   m3 -> alpha
+        '''
+        x1_1 = m1 * x1
+        x2_1 = self.conv_upsample1(self.upsample(m1 * x1)) * (m2 * x2)
+        x3_1 = self.conv_upsample2(self.upsample(x2_1)) * self.conv_upsample3(self.upsample(m2 * x2)) * m3 * x3
+
+        x2_2 = torch.cat((x2_1, self.conv_upsample4(self.upsample(x1_1))), 1)
+        x2_2 = self.conv_concat2(x2_2)
+
+        x3_2 = torch.cat((x3_1, self.conv_upsample5(self.upsample(x2_2))), 1)
+        x3_2 = self.conv_concat3(x3_2)
+
+        x = self.conv4(x3_2)
+        x = self.conv5(x)
+
+        return x

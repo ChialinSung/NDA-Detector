a81e4171eebe00dd7b0265ea2a9258d3a5edf88c
ChialinSung <songjialin35@163.com>
2024-03-18 17:17:08 +0800

    modify STEP

--- test_stepp.py
+++ test_stepp.py
@@ -1,39 +1,39 @@
 '''
-Skip Texture Enhanced Pooling Pyramid
+Skip Texture Enhanced Pooling module
 '''
 from test_network import BasicConv2d, DepthwiseSeparableConv2d
 import torch
 import torch.nn as nn
-class Stepp(nn.Module):
+class Step(nn.Module):
     def __init__(self, in_channel, out_channel):
-        super(Stepp, self).__init__()
+        super(Step, self).__init__()
         self.relu = nn.ReLU(True)
         self.up_m = nn.Sequential(
-            nn.AdaptiveAvgPool2d(128),
-            BasicConv2d(in_channel, out_channel, 1),
+            nn.AdaptiveAvgPool2d(in_channel),
+            BasicConv2d(in_channel, out_channel, kernel_size=3, dilation=5),
+            BasicConv2d(out_channel, out_channel, kernel_size=1),
             nn.Upsample(scale_factor=2, mode='nearest')
         )
         self.down_m_0 = nn.Sequential(
-            BasicConv2d(in_channel, out_channel, 1),
-            DepthwiseSeparableConv2d(out_channel, out_channel, 3, padding=1, dilation=1),
+            BasicConv2d(in_channel, out_channel, kernel_size=1),
+            BasicConv2d(out_channel, out_channel, kernel_size=3, padding=1, dilation=1)
         )
         self.down_m_1 = nn.Sequential(
-            BasicConv2d(out_channel, out_channel, 1),
-            BasicConv2d(out_channel, out_channel, kernel_size=(3, 1), padding=(1, 0)),
-            DepthwiseSeparableConv2d(out_channel, out_channel, 3, padding=3, dilation=3)
+            BasicConv2d(out_channel, out_channel, kernel_size=1),
+            DepthwiseSeparableConv2d(out_channel, out_channel, kernel_size=3),
+            BasicConv2d(out_channel, out_channel, kernel_size=3, padding=1, dilation=3)
         )
         self.down_m_2 = nn.Sequential(
-            BasicConv2d(out_channel, out_channel, 1),
-            BasicConv2d(out_channel, out_channel, kernel_size=(1, 3), padding=(0, 1)),
-            DepthwiseSeparableConv2d(out_channel, out_channel, 3, padding=5, dilation=5)
+            BasicConv2d(out_channel, out_channel, kernel_size=1),
+            DepthwiseSeparableConv2d(out_channel, out_channel, kernel_size=5),
+            BasicConv2d(out_channel, out_channel, kernel_size=3, padding=1, dilation=5)
         )
         self.down_m_3 = nn.Sequential(
-            BasicConv2d(out_channel, out_channel, 1),
-            BasicConv2d(out_channel, out_channel, kernel_size=(1, 3), padding=(0, 1)),
-            BasicConv2d(out_channel, out_channel, kernel_size=(3, 1), padding=(1, 0)),
-            DepthwiseSeparableConv2d(out_channel, out_channel, 3, padding=7, dilation=7)
+            BasicConv2d(out_channel, out_channel, kernel_size=1),
+            DepthwiseSeparableConv2d(out_channel, out_channel, kernel_size=7),
+            BasicConv2d(out_channel, out_channel, kernel_size=3, padding=1, dilation=7)
         )
-        self.conv_cat = BasicConv2d(4*out_channel, out_channel, 3, padding=1)
+        self.conv_cat = BasicConv2d(4*out_channel, out_channel, 1, padding=1)
 
     def forward(self, x):
         up_res = self.up_m(x)
